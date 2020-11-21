b19f7b9c1768c6cc0c961f3d83c28ff5b1e0cf68
Goodness Ezeokafor <gootech442@yahoo.com>
2020-11-21 14:27:45 +0100

    added

--- lib/screens/CheckoutScreen.dart
+++ lib/screens/CheckoutScreen.dart
@@ -4,11 +4,44 @@ class CheckoutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-         title:Text("Checkout Screen")
-       ),
-        body: Center(
-      child: Text("Checkout Screen"),
-    ));
+      appBar: AppBar(title: Text("Checkout Screen")),
+      body: Center(
+        child: RaisedButton(
+            child: const Text("I am a button. Press me"),
+            onPressed: () async{
+               String response = await  showDialog<String>(
+              context: context,
+              builder:(BuildContext context){
+                  return AlertDialog(
+                    content: const Text("Are you Sure? "),
+                    actions:<Widget>[
+                      FlatButton(
+                        onPressed:() => Navigator.pop(context, 'Yes'),                          child: const Text("Yes"),
+                          ),
+
+                      FlatButton(
+                        onPressed:() => Navigator.pop(context, 'No'), 
+                        child: const Text("No"))
+                    ],
+                  );
+              }
+              // builder: (BuildContext context){
+              //   return AlertDialog(
+              //     content: const Text("Press OK to checkout"),
+              //     actions:<Widget>[
+              //       FlatButton(
+              //           onPressed: () => Navigator.pop(context), 
+              //           child: const Text("OK")
+              //         )
+              //     ],
+              //   );
+              // }
+            );
+            print(response);
+            }
+             
+        ),
+      ),
+    );
   }
 }
